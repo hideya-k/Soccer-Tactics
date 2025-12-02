c9f77e1eb6a02ba5b32d767c8e964c9ad9670fb3
hideya-k <hideya0813@gmail.com>
2025-12-02 17:49:53 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Soccer_Tactics/",
+  // ↓ ここが一番重要です！前後のスラッシュ / も忘れずに。
+  base: "/Soccer-Tactics/",
 })
